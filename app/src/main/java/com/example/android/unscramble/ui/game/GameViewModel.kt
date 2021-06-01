bf90c5ad92c086432e44a4aa4df65617f503feff
Daniel (EMCT) <daniel.h@emct.com.tw>
2021-06-01 17:03:51 +0800

    Store data in ViewModel: 5. Move data to the ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,4 +6,14 @@ import androidx.lifecycle.ViewModel
  *  Created by DanielHuang on 2021/6/1
  */
 class GameViewModel : ViewModel() {
+
+    private var score = 0
+    private var currentWordCount = 0
+
+    // Backing property
+    private var _currentScrambledWord = "test"
+    // 這個讓外部存取 read-only
+    // NOTE: Never expose mutable data fields from your ViewModel
+    // NOTE: Mutable data inside the ViewModel should always be private.
+    val currentScrambledWord: String get() = _currentScrambledWord
 }
