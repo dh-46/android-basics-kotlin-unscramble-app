990cc50bafc96325dc9ec6529b98a7d254464780
Daniel (EMCT) <daniel.h@emct.com.tw>
2021-06-01 17:26:50 +0800

    Store data in ViewModel: 7. Populate ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -12,20 +12,65 @@ class GameViewModel : ViewModel() {
     private var currentWordCount = 0
 
     // Backing property
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
     // 這個讓外部存取 read-only
     // NOTE: Never expose mutable data fields from your ViewModel
     // NOTE: Mutable data inside the ViewModel should always be private.
     val currentScrambledWord: String get() = _currentScrambledWord
 
+    /**
+     * 已經出過題目的單字清單
+     */
+    private var wordsList: MutableList<String> = mutableListOf()
+
+    /**
+     * 使用者要解的字
+     */
+    private lateinit var currentWord: String
+
     init {
         // 物件初始化時執行
         Log.d("GameFragment", "GameViewModel created!")
+        // 出第一題
+        getNextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         // ViewModel被清除時執行
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    /**
+     * Logic
+     */
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        // 拆成字母
+        val tempWord = currentWord.toCharArray()
+        // 字母洗亂
+        tempWord.shuffle()
+
+        // 洗亂後的結果不能跟原本一樣
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+
+        if (wordsList.contains(currentWord)) {
+            // 已經出過該單字
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            // currentWordCount
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 }
